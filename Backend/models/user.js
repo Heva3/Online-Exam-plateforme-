9ac1e605de443a87d9aa11cc3e1f4a60a399a322
¨Aya¨ <hamou.aya@etu.uae.ac.ma>
2025-05-12 12:49:57 +0100

    user model

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,6 +1,6 @@
-// models/User.js
-
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -45,46 +45,13 @@ const userSchema = new mongoose.Schema({
     minlength: 8,
     select: false
   },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: {
-      validator: function(el) {
-        return el === this.password;
-      },
-      message: 'Passwords do not match!'
-    }
-  },
-  role: {
-    type: String,
-    enum: ['student', 'teacher', 'admin'],
-    default: 'student'
-  },
-  passwordChangedAt: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false
-  }
+  
 }, {
   timestamps: true,
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 });
 
-// Encrypt password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
-
-  if (!this.isNew) {
-    this.passwordChangedAt = Date.now() - 1000;
-  }
-
-  next();
-});
 
 // Hide inactive users from queries (optional)
 userSchema.pre(/^find/, function(next) {
@@ -107,4 +74,6 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 };
 
 const User = mongoose.model('User', userSchema);
-module.exports = User;
+export default User;
+
+
